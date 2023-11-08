00ab9d5dbb4b732287870c65bc4f2d726f337123
Taiwo Ifedayo <taiwo.ifedayo.gabriel@gmail.com>
2023-11-08 08:12:15 +0100

    push

--- pages/gamezone.js
+++ pages/gamezone.js
@@ -6,31 +6,13 @@ import { PageSEO } from '@/components/SEO'
 import useSound from 'use-sound'
 
 export default function GameZone() {
-  const [playThemeSound, { stop }] = useSound('/static/sounds/adventure.mp3', {
-    volume: 0.5, // Adjust the volume as needed (0.5 is 50% volume)
-  })
-
-  useEffect(() => {
-    playThemeSound()
-
-    return () => {
-      // This will stop the sound when the component unmounts (leaves the page)
-      stop()
-    }
-  }, [playThemeSound, stop])
-
   return (
     <>
       <PageSEO
         title={`GameZone - ${siteMetadata.author}`}
         description="Original Games I've built throughout my career, sit back, relax and have fun"
       />
-      <div
-        className="mx-auto max-w-6xl divide-y divide-gray-400"
-        style={{
-          filter: 'drop-shadow(20px 20px 50px #000)',
-        }}
-      >
+      <div className="mx-auto max-w-6xl divide-y divide-gray-400" style={{}}>
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             GameZone
@@ -43,7 +25,6 @@ export default function GameZone() {
             style={{
               width: '200px',
               margin: 'auto',
-              filter: 'drop-shadow(20px 20px 50px #0a183d)',
             }}
           />
         </div>
