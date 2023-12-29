40fa80ea61756c04e7e3421355fae7bd73a9e030
Ifedayo <nou213090168@noun.edu.ng>
2023-12-29 09:47:53 +0100

    done

--- components/Test.js
+++ components/Test.js
@@ -105,7 +105,7 @@ const Test = ({ thumb, thumbWidth, thumbHeight, thumbAlt, video, videoWidth, vid
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
             </svg>
-            <span className="ml-3 text-black dark:text-white">
+            <span className="ml-3 text-white dark:text-black">
               {videoOpen ? 'Close Video' : 'Meet me (2 min)'}
             </span>
           </button>
